074182ee0068899c74147380e7eb7ceaae36d8c3
Susan OMahony <susan.omahony@mycit.ie>
2018-04-25 14:12:33 +0100

    My to Our World

--- codebase.java
+++ codebase.java
@@ -14,7 +14,7 @@ public class HelloWorld
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello My World!");
+		System.out.println("Hello Our World!");
 	}
 >>>>>>> a395470c5751e58205dccdc2a951375b2b1fe57c
 }
