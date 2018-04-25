3c94eef8306e0299a864f66ee3a45ddf3acf77cf
Susan OMahony <susan.omahony@mycit.ie>
2018-04-25 15:42:11 +0100

    Hello Newish World!

--- codebase.java
+++ codebase.java
@@ -14,7 +14,7 @@ public class HelloWorld
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		System.out.println("Hello Newish World!");
 	}
 >>>>>>> a395470c5751e58205dccdc2a951375b2b1fe57c
 }
