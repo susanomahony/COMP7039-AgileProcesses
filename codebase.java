223522cf13a76c391de641b6e47c4e8d3d2281f8
Susan OMahony <susan.omahony@mycit.ie>
2018-04-26 13:49:11 +0100

    Bye there!

--- codebase.java
+++ codebase.java
@@ -14,7 +14,7 @@ public class HelloWorld
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hi There!");
+		System.out.println("Bye There!");
 	}
 >>>>>>> a395470c5751e58205dccdc2a951375b2b1fe57c
 }
