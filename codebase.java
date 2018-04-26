a9350c18cf2954b5beb7bcd4e3b85613ed56769c
Susan OMahony <susan.omahony@mycit.ie>
2018-04-26 15:39:05 +0100

    Allo There!

--- codebase.java
+++ codebase.java
@@ -14,7 +14,7 @@ public class HelloWorld
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hey There!");
+		System.out.println("Allo There!");
 	}
 >>>>>>> a395470c5751e58205dccdc2a951375b2b1fe57c
 }
