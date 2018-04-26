5a28672b4a9fad306ff2c9189b3cdcc88a94b4a7
Susan OMahony <susan.omahony@mycit.ie>
2018-04-26 19:58:06 +0100

    Hell Here!

--- codebase.java
+++ codebase.java
@@ -14,7 +14,7 @@ public class HelloWorld
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello Here!");
+		System.out.println("Hell Here!");
 	}
 >>>>>>> a395470c5751e58205dccdc2a951375b2b1fe57c
 }
