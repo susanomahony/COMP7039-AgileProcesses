20e7801096c36247cae9f9897889a7edef5a9065
Susan OMahony <susan.omahony@mycit.ie>
2018-04-26 20:15:38 +0100

    Hell No!

--- codebase.java
+++ codebase.java
@@ -14,7 +14,7 @@ public class HelloWorld
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hell There!");
+		System.out.println("Hell No!");
 	}
 >>>>>>> a395470c5751e58205dccdc2a951375b2b1fe57c
 }
